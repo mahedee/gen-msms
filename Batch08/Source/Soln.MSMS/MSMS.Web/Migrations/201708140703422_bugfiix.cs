843f8fb557af6d20a9ea9f58364945f381b9b8a9
git-kundu <shkundu.it@gmail.com>
2017-08-14 14:47:46 +0600

    finalday of training

---
+++ Batch08/Source/Soln.MSMS/MSMS.Web/Migrations/201708140703422_bugfiix.cs
@@ -0,0 +1,16 @@
+namespace MSMS.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class bugfiix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
